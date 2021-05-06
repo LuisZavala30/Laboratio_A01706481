abb91b2865ec74bd08ea84673fb68307c5bc85ae
Luis Angel Zavala Robles <A01706481@itesm.mx>
2021-05-06 08:41:27 -0600

    Optimización de algoritmo para calcular convolución con padding

--- convolution_padding.py
+++ convolution_padding.py
@@ -1,13 +1,13 @@
 import numpy as np
 def conv_helper(fragment, kernel):
     
-    f_row, f_col = fragment.shape
-    k_row, k_col = kernel.shape 
-    result = 0.0
-    for row in range(f_row):
-        for col in range(f_col):
-            result += fragment[row,col] *  kernel[row,col]
-    return result
+	f_row, f_col = fragment.shape
+	k_row, k_col = kernel.shape 
+	result = 0.0
+	for row in range(f_row):
+	    for col in range(f_col):
+	        result += fragment[row,col] *  kernel[row,col]
+	return result
 
 
 def convolution(image,kernel):
@@ -16,18 +16,19 @@ def convolution(image,kernel):
 	
 	image_row, image_col = image.shape 		#Asigna alto y ancho de la imagen
 	kernel_row, kernel_col = Filter.shape   #Asigna alto y ancho del filtro
-	output_x = int((image_row-(kernel_row/2)))	#Calcula dimension x de matriz de salida
-	output_y = int((image_col-(kernel_col/2)))	#Calcula dimension y de matriz de salida
-	output = np.zeros((output_x,output_y))		#Inicializa en 0 matriz de salida
 	pad_height = int((kernel_row - 1) / 2)	#Altura de padding 
 	pad_width = int((kernel_col - 1) / 2)   #Ancho de padding
+	padded_image = np.zeros((image_row + (2*pad_height),image_col + (2*pad_width)))
+	padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = image
+	padded_row, padded_col = padded_image.shape
+	output_x = int((padded_row-(kernel_row/2)))	#Calcula dimension x de matriz de salida
+	output_y = int((padded_col-(kernel_col/2)))	#Calcula dimension y de matriz de salida
+	output = np.zeros((output_x,output_y))		#Inicializa en 0 matriz de salida
 
 	for row in range(output_x):		#Tamaño de matriz de salida como maximo del ciclo
-	    for col in range(output_y):
-	        output[row, col] = conv_helper(image[row:row + kernel_row, col:col + kernel_col],kernel)
-
-	padded_image = np.pad(output, ((pad_height, pad_height), (pad_width, pad_width)), 'constant')
-	return padded_image
+		for col in range(output_y):
+			output[row, col] = conv_helper(padded_image[row:row + kernel_row, col:col + kernel_col],kernel)
+	return output
 
 
 
@@ -45,6 +46,6 @@ image = np.array([	[1,2,3,4,5,6],		#Matriz o imagen
 					[1,7,6,5,4,3],
 	])
 
-	
+
 
 print(convolution(image,Filter))
